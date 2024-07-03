906bd32e74a34dcac7e3ba1153d4118643495b2b
kimruafase <ppooppplus33@gmail.com>
2024-07-03 16:24:55 +0900

    20240703 16:24

--- src/AppStart.java
+++ src/AppStart.java
@@ -1,8 +1,9 @@
+import view.DungeonView;
 import view.MyAccountView;
 
 public class AppStart {
     public static void main(String[] args) {
-        MyAccountView.getinstance().index();
+        MyAccountView.getInstance().index();
 
     }
 }
